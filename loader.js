7c04dc9351e9b6632c5f65d6edf2d097f7a9e2df
Ian-Ibrahim <77841547+Ian-Ibrahim@users.noreply.github.com>
2021-03-11 17:35:03 +0300

    Delete loader.js

--- loader.js
+++
@@ -1,6 +0,0 @@
-myVar = setTimeout(showPage, 4800);
-
-function showPage() {
-document.getElementById("loader").style.display = "none";
-document.getElementById("myBody").style.display = "block";
-}
